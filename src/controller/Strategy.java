f7916a9edb00424621d0d17f37f43d9516185f87
kai_Ker <kai_Ker@buaa.edu.cn>
2024-03-30 13:22:16 +0800

    [Opti] Refractor the strategy to avoid closing-opening

--- src/controller/Strategy.java
+++ src/controller/Strategy.java
@@ -14,51 +14,65 @@ public class Strategy {
     public static ElevatorStrategyType elevatorStrategy(
             ArrayList<PassageRequest> requests, ArrayList<PassageRequest> onboards,
             int floor, ElevatorDirection direction) {
-        if (needOpenOut(onboards, floor) || needOpenIn(requests, onboards, floor, direction)) {
-            return ElevatorStrategyType.OPEN;
+        // If someone want to leave, must open the door!
+        if (needOpenOut(onboards, floor)) {
+            if (hasRequestSameDirectionThisFloor(requests, floor, direction)) {
+                // Passenger with the same direction
+                return ElevatorStrategyType.OPEN;
+            }
+            if (hasRequestsAhead(requests, floor, direction)) {
+                // Have requests ahead
+                return ElevatorStrategyType.OPEN;
+            }
+            // No requests ahead and No passengers at the same direction
+            return ElevatorStrategyType.REVISE_OPEN;
         }
         // vvv No passengers want to leave vvv
-        if (!onboards.isEmpty()) {
-            // Have passengers onboard, move directly
+        if (onboards.size() == ElevatorLimits.MAX_PASSENGER) {
+            // Full of passengers, move directly
             return ElevatorStrategyType.MOVE;
-        }
-        // vvv No people onboard vvv
-        if (requests.isEmpty()) {
-            // No requests in queue, wait
-            return ElevatorStrategyType.WAIT;
-        }
-        // vvv Have requests somewhere vvv
-        boolean requestSameDirection = false;
-        // boolean requestReverseDirection = false;
-        boolean requestGoReversely = false;
-        for (PassageRequest request : requests) {
-            ElevatorDirection due = ElevatorDirection.construct(floor, request.getFromFloor());
-            if (request.getFromFloor() == floor) {
-                // It must be reversed
-                requestGoReversely = true;
-            } else if (direction == due) {
-                requestSameDirection = true;
-            } /* else {
-                requestReverseDirection = true;
-            } */
-        }
-        if (requestSameDirection) {
-            return ElevatorStrategyType.MOVE;
-        } else if (requestGoReversely) {
-            return ElevatorStrategyType.REVISE_OPEN;
-        } else /*if (requestReverseDirection)*/ {
+        } else if (onboards.isEmpty()) {
+            // No passengers onboard
+            if (requests.isEmpty()) {
+                // No requests in queue, wait
+                return ElevatorStrategyType.WAIT;
+            }
+            // vvv Have requests somewhere vvv
+            if (hasRequestSameDirectionThisFloor(requests, floor, direction)) {
+                return ElevatorStrategyType.OPEN;
+            }
+            if (hasRequestsAhead(requests, floor, direction)) {
+                return ElevatorStrategyType.MOVE;
+            }
+            // No requests ahead and No passengers at the same direction
+            if (hasRequestDiffDirectionThisFloor(requests, floor, direction)) {
+                return ElevatorStrategyType.REVISE_OPEN;
+            }
             return ElevatorStrategyType.REVISE_MOVE;
+        } else {
+            // Have passengers onboard, but not full
+            if (hasRequestSameDirectionThisFloor(requests, floor, direction)) {
+                return ElevatorStrategyType.OPEN;
+            }
+            return ElevatorStrategyType.MOVE;
         }
     }
 
-    private static boolean needOpenIn(
-            ArrayList<PassageRequest> requests, ArrayList<PassageRequest> onboards,
-            int floor, ElevatorDirection direction) {
+    private static boolean hasRequestSameDirectionThisFloor(
+            ArrayList<PassageRequest> requests, int floor, ElevatorDirection direction) {
         for (PassageRequest request : requests) {
             if (request.getFromFloor() == floor && request.sameDirection(direction)) {
-                if (onboards.size() < ElevatorLimits.MAX_PASSENGER) {
-                    return true;
-                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean hasRequestDiffDirectionThisFloor(
+            ArrayList<PassageRequest> requests, int floor, ElevatorDirection direction) {
+        for (PassageRequest request : requests) {
+            if (request.getFromFloor() == floor && !request.sameDirection(direction)) {
+                return true;
             }
         }
         return false;
@@ -71,5 +85,32 @@ public class Strategy {
             }
         }
         return false;
+    }
+
+    private static boolean hasRequestsDown(ArrayList<PassageRequest> requests, int floor) {
+        for (PassageRequest request : requests) {
+            if (request.getFromFloor() < floor) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean hasRequestsUp(ArrayList<PassageRequest> requests, int floor) {
+        for (PassageRequest request : requests) {
+            if (request.getFromFloor() > floor) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean hasRequestsAhead(
+            ArrayList<PassageRequest> requests, int floor, ElevatorDirection direction) {
+        if (direction == ElevatorDirection.UP) {
+            return hasRequestsUp(requests, floor);
+        } else {
+            return hasRequestsDown(requests, floor);
+        }
     }
 }
